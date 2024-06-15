990c4d09e13e4fe5bfd3e46c2a260e18ba65a0a0
Matías Ezequiel Gil <89765@sistemas.frc.utn.edu.ar>
2024-06-15 17:28:34 -0300

    Añadir /api en todos los endpoints

--- backend/routes/countries.routes.js
+++ backend/routes/countries.routes.js
@@ -4,11 +4,11 @@ const countryRouter = express.Router();
 // Service functions
 import countryService from "../services/country.service.js";
 
-countryRouter.get("/", countryService.getCountries);
-countryRouter.get("/:id", countryService.getCountryById);
-countryRouter.get("/search", countryService.getCountryByName);
-countryRouter.post("/", countryService.createCountry);
-countryRouter.put("/:id", countryService.updateCountry);
-countryRouter.delete("/:id", countryService.deleteCountry);
+countryRouter.get("/api/", countryService.getCountries);
+countryRouter.get("/api/:id", countryService.getCountryById);
+countryRouter.get("/api/search", countryService.getCountryByName);
+countryRouter.post("/api/", countryService.createCountry);
+countryRouter.put("/api/:id", countryService.updateCountry);
+countryRouter.delete("/api/:id", countryService.deleteCountry);
 
 export default countryRouter;
